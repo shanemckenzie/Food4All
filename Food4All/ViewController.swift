be28844eabf5bc884b08999952ae044826c904d9
Shane McKenzie <shane.mckenzie59@gmail.com>
2017-01-26 13:05:41 -0600

    Initial Commit

---
+++ Food4All/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Food4All
+//
+//  Created by Shane Mckenzie on 1/26/17.
+//  Copyright © 2017 University of Regina (Department of Computer Science). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Food4AllTests/Food4AllTests.swift
@@ -0,0 +1,36 @@
+//
+//  Food4AllTests.swift
+//  Food4AllTests
+//
+//  Created by Shane Mckenzie on 1/26/17.
+//  Copyright © 2017 University of Regina (Department of Computer Science). All rights reserved.
+//
+
+import XCTest
+@testable import Food4All
+
+class Food4AllTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
