3821c3213e17667e81db714e40b80cdbe652da02
bill <bill@bills-Mac.local>
2017-03-07 19:48:36 -0600

    Added user authentication and registration

---
+++ Food4All/InitialViewController.swift
@@ -0,0 +1,85 @@
+//
+//  InitialViewController.swift
+//  Food4All
+//
+//  Created by bill on 3/7/17.
+//  Copyright © 2017 University of Regina (Department of Computer Science). All rights reserved.
+//
+
+import UIKit
+import Firebase
+import FirebaseAuth
+import os.log
+
+
+class InitialViewController: UIViewController {
+
+    //MARK: PROPERTIES
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
+    //MARK: ACTIONS
+    
+    @IBAction func userLogInAction(_ sender: UIButton) {
+        //Authenticate user with firebase
+        FIRAuth.auth()?.signIn(withEmail: emailField.text!, password: passwordField.text!) { (user, error) in
+            if let error = error {
+               
+                let showMessagePrompt = UIAlertController(title: "Log In Error", message: error.localizedDescription, preferredStyle: UIAlertControllerStyle.alert)
+                
+                showMessagePrompt.addAction(UIAlertAction(title: "OK", style: .default, handler: { (action: UIAlertAction!) in
+                }))
+                
+                self.present(showMessagePrompt, animated: true, completion: nil)
+                
+                return
+            }
+            else{
+                print("USER HAS BEEN AUTHENTICATED")
+                self.performSegue(withIdentifier: "logInSegue", sender: nil)
+            }
+        }
+    }
+    
+    @IBAction func ResgisterUserAction(_ sender: UIButton) {
+        FIRAuth.auth()?.createUser(withEmail: emailField.text!, password: passwordField.text!) { (user, error) in
+            if let error = error {
+                
+                let showMessagePrompt = UIAlertController(title: "Registration Error", message: error.localizedDescription, preferredStyle: UIAlertControllerStyle.alert)
+                
+                showMessagePrompt.addAction(UIAlertAction(title: "OK", style: .default, handler: { (action: UIAlertAction!) in
+                }))
+                
+                self.present(showMessagePrompt, animated: true, completion: nil)
+                
+                return
+            }
+            else{
+                print("USER HAS BEEN REGISTERED")
+            }
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Podfile
+++ Podfile
@@ -3,10 +3,13 @@
 
 target 'Food4All' do
   # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
-  use_frameworks!
+  #use_frameworks!
 
   # Pods for Food4All
   pod 'Firebase/Core'
+  
+  # For user accounts
+  pod 'Firebase/Auth'
 
   target 'Food4AllTests' do
     inherit! :search_paths
