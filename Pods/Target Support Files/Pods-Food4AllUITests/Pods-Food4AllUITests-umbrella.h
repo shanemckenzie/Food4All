c422e5d18ce313fdbaa746a2bde854009638df59
Shane McKenzie <shane.mckenzie59@gmail.com>
2017-03-11 16:24:48 -0600

    Updated UI

--- Pods/Target Support Files/Pods-Food4AllUITests/Pods-Food4AllUITests-umbrella.h
+++
@@ -1,8 +0,0 @@
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#endif
-
-
-FOUNDATION_EXPORT double Pods_Food4AllUITestsVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_Food4AllUITestsVersionString[];
-
