f239976d1af02ec316c5cac3ff00cec9254a8a8c
bill <bill@bills-Mac.local>
2017-03-14 20:17:40 -0600

    Forgot to uncomment the side bar in the last commit

--- Food4All/HomeViewController.swift
+++ Food4All/HomeViewController.swift
@@ -13,7 +13,7 @@ class HomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        //self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
         
     }
 
