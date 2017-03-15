a90541a326dbdeb2ec3e81e765f18a029c65a0a5
Shane McKenzie <shane.mckenzie59@gmail.com>
2017-03-15 10:17:20 -0600

    GPS view zooms on user location

---
+++ Food4All/GPSViewController.swift
@@ -0,0 +1,106 @@
+//
+//  GPSViewViewController.swift
+//  Food4All
+//
+//  Created by Shane Mckenzie on 2/4/17.
+//  Copyright © 2017 University of Regina (Department of Computer Science). All rights reserved.
+//
+
+import UIKit
+import MapKit
+import CoreLocation
+
+
+class GPSViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
+
+    @IBOutlet weak var mapView: MKMapView!
+    
+    let regionRadius: CLLocationDistance = 1000
+    let locationManager = CLLocationManager()
+    var myLocation: CLLocationCoordinate2D?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        self.mapView.showsUserLocation = true
+        
+        // Ask for Authorisation from the User.
+        self.locationManager.requestAlwaysAuthorization()
+        
+        // For use in foreground
+        self.locationManager.requestWhenInUseAuthorization()
+        
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+            locationManager.startUpdatingLocation()
+        }
+        
+//        let myLocation = locationManager.location!.coordinate
+//        centerMap(myLocation)
+        
+        //centerMapOnLocation(location: locationManager.location?)
+        //centerMapOnLocation(location: (locationManager.location)!)
+        
+        mapView.delegate = self
+        mapView.mapType = .standard
+        mapView.isZoomEnabled = true
+        mapView.isScrollEnabled = true
+        
+        if let coor = mapView.userLocation.location?.coordinate {
+            mapView.setCenter(coor, animated: true)
+        }
+        
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        mapView.showsUserLocation = true;
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+//    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+//        var locValue:CLLocationCoordinate2D = manager.location.coordinate
+//        print("locations = \(locValue.latitude) \(locValue.longitude)")
+//    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let locValue:CLLocationCoordinate2D = manager.location!.coordinate
+        
+        centerMap(locValue)
+    }
+    
+    func centerMap(_ center:CLLocationCoordinate2D){
+        //self.saveCurrentLocation(center)
+        
+        let spanX = 0.007
+        let spanY = 0.007
+        
+        let newRegion = MKCoordinateRegion(center:center , span: MKCoordinateSpanMake(spanX, spanY))
+        mapView.setRegion(newRegion, animated: true)
+    }
+    
+    
+//    func centerMapOnLocation(location: CLLocation) {
+//        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate, regionRadius * 2.0, regionRadius * 2.0)
+//        mapView.setRegion(coordinateRegion, animated: true)
+//    }
+
+}
---
+++ Food4All/MapData.swift
@@ -0,0 +1,9 @@
+//
+//  MapData.swift
+//  Food4All
+//
+//  Created by Shane Mckenzie on 3/14/17.
+//  Copyright © 2017 University of Regina (Department of Computer Science). All rights reserved.
+//
+
+import Foundation
