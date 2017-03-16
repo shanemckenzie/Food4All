f5d3b382cc5a739541c08b9fa7bc3d2619d2eb8e
bill <bill@bills-Mac.local>
2017-03-16 15:32:54 -0600

    created model class to manage data

---
+++ Food4All/DonatedItems.swift
@@ -0,0 +1,97 @@
+//
+//  DonatedItems.swift
+//  Food4All
+//
+//  Created by bill on 3/16/17.
+//  Copyright © 2017 University of Regina (Department of Computer Science). All rights reserved.
+//
+
+import CoreLocation
+import Foundation
+import Firebase
+import FirebaseAuth
+
+class DonatedItems: NSObject{
+    
+    //MARK: Properties
+    var donatedItems = [DonatedItem]()
+    
+    //MARK: Piblic functions
+    
+    func initItems(){
+        loadSampleDonation()
+    }
+    
+    func getCount () -> Int{
+        return donatedItems.count
+    }
+    
+    func getItem(index: Int) -> DonatedItem{
+        return donatedItems[index]
+    }
+    
+    func addItem(item: DonatedItem){
+        donatedItems.append(item)
+    }
+    
+    //MARK: UPDATE FOR SORTING
+    func updateItem(item: DonatedItem, index: Int){
+        
+        //remove item from index
+        donatedItems.remove(at: index)
+        
+        //replace item at index
+        donatedItems.append(item)
+    }
+    
+    /*
+     
+     func updateItem(item: Item, index: Int){
+     //items[index] = item
+     
+     //remeove thee item, then readd it so the list remains sorted
+     removeItem(index: index)
+     addItem(item: item)
+     }
+     
+     
+     func removeItem(index: Int){
+     items.remove(at: index)
+     }
+     */
+    
+    
+    //MARK: Private Functions
+    private func loadSampleDonation() {
+        
+        let photo = UIImage(named: "defaultPhoto")
+        let title = "Sample Donation"
+        let description = "Sample donation description"
+        let title2 = "Sample Donation Request"
+        let description2 = "Requesting donations of ______"
+        let date = NSDate()
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "MMMM dd, h:mm a"
+        
+        let dateString: String = formatter.string(from: date as Date)
+        let coordinates = CLLocationCoordinate2D(latitude: 50.417433, longitude: -104.594179)
+        let coordinates2 = CLLocationCoordinate2D(latitude: 50.417439, longitude: -104.59417)
+        
+        
+        guard let donation1 = DonatedItem(title, photo!, true, description, dateString, coordinates) else {
+            fatalError("Unable to instantiate object")
+        }
+        
+        guard let donation2 = DonatedItem(title2, photo!, false, description2, dateString, coordinates2) else {
+            fatalError("Unable to instantiate object")
+        }
+        
+        
+        donatedItems += [donation1, donation2]
+        
+        
+    }
+    
+    
+}
--- Food4All/HomeViewController.swift
+++ Food4All/HomeViewController.swift
@@ -14,22 +14,22 @@ import os.log
 
 class HomeViewController: UITableViewController {
     
-    var donatedItems = [DonatedItem]()
+    let donatedItems = DonatedItems();
     var ref: FIRDatabaseReference!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        //load sample data
+        donatedItems.initItems()
+        
         self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
         
         //TODO: Load data from database
         //create object, load the data in object as array
-        
-        loadSampleDonation()
-        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -42,7 +42,7 @@ class HomeViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return donatedItems.count
+        return donatedItems.getCount()
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -54,7 +54,7 @@ class HomeViewController: UITableViewController {
                 fatalError("The dequeued cell is not an instance of ToDoItemCell")
         }
         
-        let item = donatedItems[indexPath.row]
+        let item = donatedItems.getItem(index: indexPath.row)
         print("Loading cells")
         
         //TODO: DO assign values to cell
@@ -73,69 +73,95 @@ class HomeViewController: UITableViewController {
         
         os_log("Due date changed ", log: OSLog.default, type: .debug)
         
-//        if toDo.enteredDate != nil {
-//            cell.toDoDateEntered.text = "Date Entered: \(formatter.string(from: toDo.enteredDate as! Date))"
-//        }
+        //        if toDo.enteredDate != nil {
+        //            cell.toDoDateEntered.text = "Date Entered: \(formatter.string(from: toDo.enteredDate as! Date))"
+        //        }
         
         //cell.toDoDateDue.text = toDo.dueDate
         
-//        if toDo.notes != "" {
-//            cell.toDoNotes.text = "Notes: \(toDo.notes!)"
-//        } else {
-//            cell.toDoNotes.text = "Notes:"
-//        }
-//        print("Priority: \(toDo.priority)")
-//        
-//        if toDo.priority! == 1 {
-//            cell.layer.backgroundColor = UIColor(red: 255/255, green: 51/255, blue: 17/255, alpha: 0.4).cgColor
-//        } else if toDo.priority! == 2 {
-//            cell.layer.backgroundColor = UIColor(red: 255/255, green: 255/255, blue: 102/255, alpha: 0.4).cgColor
-//        } else if toDo.priority! == 3 {
-//            cell.layer.backgroundColor = UIColor(red: 94/255, green: 255/255, blue: 151/255, alpha: 0.4).cgColor
-//        }
+        //        if toDo.notes != "" {
+        //            cell.toDoNotes.text = "Notes: \(toDo.notes!)"
+        //        } else {
+        //            cell.toDoNotes.text = "Notes:"
+        //        }
+        //        print("Priority: \(toDo.priority)")
+        //
+        //        if toDo.priority! == 1 {
+        //            cell.layer.backgroundColor = UIColor(red: 255/255, green: 51/255, blue: 17/255, alpha: 0.4).cgColor
+        //        } else if toDo.priority! == 2 {
+        //            cell.layer.backgroundColor = UIColor(red: 255/255, green: 255/255, blue: 102/255, alpha: 0.4).cgColor
+        //        } else if toDo.priority! == 3 {
+        //            cell.layer.backgroundColor = UIColor(red: 94/255, green: 255/255, blue: 151/255, alpha: 0.4).cgColor
+        //        }
         
         return cell
     }
     
     // MARK: - Navigation
-
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destinationViewController.
-     // Pass the selected object to the new view controller.
-     
+    
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+        
         super.prepare(for: segue,sender: sender)
-     
-         switch(segue.identifier ?? "") {
-         //the add item button is pressed
-            case "AddItem":
+        
+        switch(segue.identifier ?? "") {
+        //the add item button is pressed
+        case "AddItem":
             os_log("Adding a new donation.", log: OSLog.default, type: .debug)
-         
-            //an existing item is pressed
-            case "ShowDetail":
-                guard let SubmissionVC = segue.destination as? SubmissionVC else {
-                    fatalError("Unexpected destination: \(segue.destination)")
-                }
-         
-                guard let selectedDonationItemCell = sender as? DonationItemCell else {
-                    fatalError("Unexpected sender: \(sender)")
-                }
-         
-                guard let indexPath = tableView.indexPath(for: selectedDonationItemCell) else {
-                    fatalError("The selected cell is not being displayed by the table")
-                }
-         
-                let selectedDonation = donatedItems[indexPath.row]
-                //toDoDetailViewController.toDoItem = selectedToDo
             
-                //TODO: load this to SubmissionVC
-                SubmissionVC.donatedItem = selectedDonation
-         
-            default:
-                fatalError("Unexpected Segue Identifier; \(segue.identifier)")
+        //an existing item is pressed
+        case "ShowDetail":
+            guard let SubmissionVC = segue.destination as? SubmissionVC else {
+                fatalError("Unexpected destination: \(segue.destination)")
             }
+            
+            guard let selectedDonationItemCell = sender as? DonationItemCell else {
+                fatalError("Unexpected sender: \(sender)")
+            }
+            
+            guard let indexPath = tableView.indexPath(for: selectedDonationItemCell) else {
+                fatalError("The selected cell is not being displayed by the table")
+            }
+            
+            let selectedDonation = donatedItems.getItem(index: indexPath.row)
+            //toDoDetailViewController.toDoItem = selectedToDo
+            
+            //TODO: load this to SubmissionVC
+            SubmissionVC.donatedItem = selectedDonation
+            
+        default:
+            fatalError("Unexpected Segue Identifier; \(segue.identifier)")
+        }
+        
+    }
+    /*
      
+     @IBAction func unwindToMealList(sender: UIStoryboardSegue) {
+     if let sourceViewController = sender.source as? ViewController, let item = sourceViewController.item {
+     
+     if let selectedIndexPath = tableView.indexPathForSelectedRow {
+     // Update an existing item.
+     itemsList.updateItem(item: item, index: selectedIndexPath.row)
+     //update all rows since we don't know where the sorted item ends up
+     tableView.reloadData()
      }
+     else {
+     // Add a new item.
+     
+     itemsList.addItem(item: item)
+     let newIndexPath = IndexPath(row: itemsList.getIndex(item: item), section: 0)
+     
+     tableView.insertRows(at: [newIndexPath], with: .automatic)
+     }
+     // Save the items.
+     itemsList.saveItems()
+     }
+     
+     
+     */
+    
     
     //MARK: Actions
     @IBAction func unwindToToDoList(sender: UIStoryboardSegue) {
@@ -146,15 +172,17 @@ class HomeViewController: UITableViewController {
             os_log("saving edited or new item", log: OSLog.default, type: .debug)
             
             if let selectedIndexPath = tableView.indexPathForSelectedRow {
-                donatedItems[selectedIndexPath.row] = donatedItem
+                donatedItems.updateItem(item: donatedItem, index: selectedIndexPath.row)
+                
+                //donatedItems[selectedIndexPath.row] = donatedItem
                 tableView.reloadRows(at: [selectedIndexPath], with: .none)
                 tableView.reloadData()
                 
             } else {
-                let newIndexPath = IndexPath(row: donatedItems.count, section: 0)
+                let newIndexPath = IndexPath(row: donatedItems.getCount(), section: 0)
                 
-                donatedItems.append(donatedItem)
-
+                donatedItems.addItem(item: donatedItem)
+                
                 
                 tableView.insertRows(at: [newIndexPath], with: .automatic)
                 tableView.reloadData()
@@ -165,88 +193,55 @@ class HomeViewController: UITableViewController {
             //saveDonation()
         }
     }
-
     
-//    private func loadSampleToDo() {
-//        
-//        let photo = UIImage(named: "defaultPhoto")
-//        
-//        guard let toDo1 = ToDoItem("Get milk", photo, "go to store get milk", "Due Immediately", NSDate() , NSDate(), 1) else {
-//            fatalError("Unable to instantiate todoitem1")
-//        }
-//        
-//        toDoItems += [toDo1]
-//        sortedList = TableSort(toSort: toDoItems)
-//        toDoItems = (sortedList?.sortedList)!
-//        
-//    }
-
     
-    //MARK: Private Methods
-    private func loadSampleDonation() {
-        
-        let photo = UIImage(named: "defaultPhoto")
-        let title = "Sample Donation"
-        let description = "Sample donation description"
-        let title2 = "Sample Donation Request"
-        let description2 = "Requesting donations of ______"
-        let date = NSDate()
-        
-        let formatter = DateFormatter()
-        formatter.dateFormat = "MMMM dd, h:mm a"
-        
-        let dateString: String = formatter.string(from: date as Date)
-        let coordinates = CLLocationCoordinate2D(latitude: 50.417433, longitude: -104.594179)
-        let coordinates2 = CLLocationCoordinate2D(latitude: 50.417439, longitude: -104.59417)
-
-        
-        guard let donation1 = DonatedItem(title, photo!, true, description, dateString, coordinates) else {
-            fatalError("Unable to instantiate object")
-        }
-        
-        guard let donation2 = DonatedItem(title2, photo!, false, description2, dateString, coordinates2) else {
-            fatalError("Unable to instantiate object")
-        }
-        
-        
-        donatedItems += [donation1, donation2]
-        
-        
-    }
-
-//    private func saveDonation() {
-//        //let isSuccessfulSave = NSKeyedArchiver.archiveRootObject(toDoItems, toFile: ToDoItem.ArchiveURL.path)
-//       
-//        ref = FIRDatabase.database().reference()
-//        
-//        let newDonationItemRef = self.ref!.child("DonationItem").childByAutoId()
-//        
-//        let newDonationItemId = newDonationItemRef.key
-//        
-//        let newDonationItemData: [String : Any] = ["itemID": newDonationItemId,
-//            "title": title as NSString,
-//            "description": description as NSString,
-//             "expiration": date as NSString
-//            //user
-//            //image
-//            //location
-//        ]
-//        
-//        newDonationItemRef.setValue(newDonationItemData)
+    //    private func loadSampleToDo() {
+    //
+    //        let photo = UIImage(named: "defaultPhoto")
+    //
+    //        guard let toDo1 = ToDoItem("Get milk", photo, "go to store get milk", "Due Immediately", NSDate() , NSDate(), 1) else {
+    //            fatalError("Unable to instantiate todoitem1")
+    //        }
+    //
+    //        toDoItems += [toDo1]
+    //        sortedList = TableSort(toSort: toDoItems)
+    //        toDoItems = (sortedList?.sortedList)!
+    //
+    //    }
     
-//        if isSuccessfulSave {
-//            os_log("toDoItems saved.", log: OSLog.default, type: .debug)
-//            
-//        } else {
-//            os_log("failed to save toDoItems", log: OSLog.default, type: .error)
-//        }
-        
-//    }
-//
-//    private func loadToDo() -> [ToDoItem]? {
-//        return NSKeyedUnarchiver.unarchiveObject(withFile: ToDoItem.ArchiveURL.path) as? [ToDoItem]
-//        
-//    }
-
-
+    //    private func saveDonation() {
+    //        //let isSuccessfulSave = NSKeyedArchiver.archiveRootObject(toDoItems, toFile: ToDoItem.ArchiveURL.path)
+    //
+    //        ref = FIRDatabase.database().reference()
+    //
+    //        let newDonationItemRef = self.ref!.child("DonationItem").childByAutoId()
+    //
+    //        let newDonationItemId = newDonationItemRef.key
+    //
+    //        let newDonationItemData: [String : Any] = ["itemID": newDonationItemId,
+    //            "title": title as NSString,
+    //            "description": description as NSString,
+    //             "expiration": date as NSString
+    //            //user
+    //            //image
+    //            //location
+    //        ]
+    //
+    //        newDonationItemRef.setValue(newDonationItemData)
+    
+    //        if isSuccessfulSave {
+    //            os_log("toDoItems saved.", log: OSLog.default, type: .debug)
+    //
+    //        } else {
+    //            os_log("failed to save toDoItems", log: OSLog.default, type: .error)
+    //        }
+    
+    //    }
+    //
+    //    private func loadToDo() -> [ToDoItem]? {
+    //        return NSKeyedUnarchiver.unarchiveObject(withFile: ToDoItem.ArchiveURL.path) as? [ToDoItem]
+    //        
+    //    }
+    
+    
 }
